24ff1b1d8c757ba3d14c39823986e0bec7cc0ea9
Sem-SO <analog9319@gmail.com>
2021-06-14 21:22:24 +0300

    Пример тестирования сайта на разных языках

--- test_items.py
+++ test_items.py
@@ -1,6 +1,5 @@
 def test_bascket_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    button = browser.find_elements_by_css_selector('.btn-add-to-66666basket')
-
+    button = browser.find_elements_by_css_selector('.btn-add-to-basket')
     assert len(button) > 0, 'такой кнопки не существует'
     print("Тест завершен")
